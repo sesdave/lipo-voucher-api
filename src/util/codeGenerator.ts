68f0faa7d27db502f29485624813ab14aa3503dd
David <david.tyohemba@vendorcredit.ng>
2024-05-29 12:52:08 +0100

    added data validation

--- src/util/codeGenerator.ts
+++ src/util/codeGenerator.ts
@@ -2,6 +2,7 @@ import { randomBytes } from 'crypto';
 
 const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const codeLength = 16;
+const MAX_ATTEMPTS = 20;
 
 export function generateRandomCode(): string {
   return Array.from(randomBytes(codeLength))
@@ -12,10 +13,16 @@ export function generateRandomCode(): string {
 export async function generateUniqueVoucherCode(offensiveWords: Set<string>, checkCodeExists: (code: string) => Promise<boolean>): Promise<string> {
   let code: string;
   let codeExists: boolean;
+  let attempt: number = 0;
 
   do {
+    if(attempt >= MAX_ATTEMPTS){
+      console.log(`Unable to generate Voucher after 20 attempts`)
+      throw new Error("Unable to generate Voucher")
+    }
     code = generateRandomCode();
     codeExists = await checkCodeExists(code);
+    attempt++
   } while (offensiveWords.has(code) || codeExists);
 
   return code;
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,8 +1,22 @@
+import { isAfter, parseISO } from 'date-fns';
+
 export function parseRequestBody(event: any): any {
     return JSON.parse(event.body || '{}');
 }
   
 export function validateVoucherData(value: any, expiryDate: any): boolean {
-    return value && expiryDate;
+    if (!value) {
+        return false;
+    }
+
+    const currentDate = new Date();
+    const expiry = parseISO(expiryDate);
+
+    if (isNaN(expiry.getTime())) {
+        console.log(`Invalid Expiry date - ${expiryDate}`)
+        return false;
+    }
+
+    return isAfter(expiry, currentDate);
 }
   
